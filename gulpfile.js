54499f34e1e15e8fcef0230aa76d5b2eed243ff8
websamrus <websamrus@gmail.com>
2019-11-26 02:34:54 +0200

    feat: fix header

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ let cssmin = require('gulp-cssmin');
 
  
 gulp.task('sass', function () {
-  return gulp.src('app/scss/style.scss')
+  return gulp.src('app/scss/**/*.scss')
           .pipe(sass({outputStyle: 'compressed'}))
           .pipe(rename({suffix : '.min'}))
           .pipe(autoprefixer({
@@ -61,7 +61,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
   gulp.watch('app/*.html', gulp.parallel('html'));
   gulp.watch('app/js/*.js', gulp.parallel('js'));
 });
