9c60fe06f0c52ed7b9bbcaeb6bb61e65d31ab259
websamrus <websamrus@gmail.com>
2019-12-01 13:28:30 +0200

    feat: fix section products

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+      });
+
+      var mixer = mixitup('.products__box');
+
 });
