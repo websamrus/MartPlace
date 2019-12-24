df0398384df6787df7d9d6aefd0ceb18d3ca7ae6
websamrus <websamrus@gmail.com>
2019-12-24 18:45:19 +0200

    feat: fix project

--- app/js/main.js
+++ app/js/main.js
@@ -4,95 +4,108 @@ $(function() {
         rating: 5,
         starWidth: "12px",
         readOnly: true
-      });
-      $(".rate__star").rateYo({
+    });
+    $(".rate__star").rateYo({
         rating: 4.8,
         starWidth: "17px",
         readOnly: true
-      });
+    });
 
-      $(".items__slider").slick({
+    $(".items__slider").slick({
         responsive: [{
-          breakpoint: 600,
-          settings: {
-              arrows: false,
-              autoplay: true,
-              autoplaySpeed: 3000,
-          }
-      }]
-      })
+            breakpoint: 600,
+            settings: {
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+            }
+        }]
+    })
 
-      $(".followers__slider").slick({
+    $(".followers__slider").slick({
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        variableWidth: true, 
+        variableWidth: true,
         responsive: [{
-          breakpoint: 1190,
-          settings: {
-            slidesToShow: 2,
-            variableWidth: false, 
-          }
-      },
-      {
-          breakpoint: 800,
-          settings: {
-            slidesToShow: 1,
-            variableWidth: false, 
-            arrows: false,
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 3000,
-          }
-    },
-    ]
+                breakpoint: 1190,
+                settings: {
+                    slidesToShow: 2,
+                    variableWidth: false,
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    variableWidth: false,
+                    arrows: false,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                }
+            },
+        ]
 
-      })
+    })
 
-      
+
     $(".js-range-slider").ionRangeSlider({
-      type: "double",
-      min: 0,
-      max: 350,
-      from: 30,
-      to: 300,
-      prefix: "$"
-  });
-
-  $('.filter__select-list').on('click', function(){
-    $('.products__item').addClass('list__item');
-    $('.filter__select-list').addClass('active');
-    $('.filter__select-grid').removeClass('active');
-  });
-
-  $('.filter__select-grid').on('click', function(){
-    $('.products__item').removeClass('list__item');
-    $('.filter__select-grid').addClass('active');
-    $('.filter__select-list').removeClass('active');
-  });
-
-  $('.header__menu-btn').on('click', function(){
-    $('.header__menu-list').slideToggle();
-  });
-
-  $('.header__user-menu').on('click', function(){
-    $('.header__user').toggleClass('active');
-  });
-
-  $('.drop-down').on('click',function(){
-    $(this).children('.drop-down__list').toggleClass('active');
-  });
-
-  $('.details__tabs .tab').on('click', function(event) {
-    var id = $(this).attr('data-id');
-      $('.details__tabs').find('.tab-item').removeClass('active-tab').hide();
-      $('.details__tabs .tabs').find('.tab').removeClass('active');
-      $(this).addClass('active');
-      $('#'+id).addClass('active-tab').fadeIn();
-      return false;
+        type: "double",
+        min: 0,
+        max: 350,
+        from: 30,
+        to: 300,
+        prefix: "$"
     });
 
-      var mixer = mixitup('.products__box');
+    $('.filter__select-list').on('click', function() {
+        $('.products__item').addClass('list__item');
+        $('.filter__select-list').addClass('active');
+        $('.filter__select-grid').removeClass('active');
+    });
+
+    $('.filter__select-grid').on('click', function() {
+        $('.products__item').removeClass('list__item');
+        $('.filter__select-grid').addClass('active');
+        $('.filter__select-list').removeClass('active');
+    });
+
+    $('.header__menu-btn').on('click', function() {
+        $('.header__menu-list').slideToggle();
+    });
+
+    $('.header__user-menu').on('click', function() {
+        $('.header__user').toggleClass('active');
+    });
+
+    $('.drop-down').on('click', function() {
+        $(this).children('.drop-down__list').toggleClass('active');
+    });
+
+    $('.details__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.details__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.details__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+    $('.single').on('click', function() {
+        $(this).children('.price__title').addClass('active');
+        $(this).children('.price__text').addClass('active');
+        $('.multi').children('.price__title').removeClass('active');
+        $('.multi').children('.price__text').removeClass('active');
+    });
+    $('.multi').on('click', function() {
+        $(this).children('.price__title').addClass('active');
+        $(this).children('.price__text').addClass('active');
+        $('.single').children('.price__title').removeClass('active');
+        $('.single').children('.price__text').removeClass('active');
+    });
+
+    var mixer = mixitup('.products__box');
 
 
 
